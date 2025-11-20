877e4576fba8759f1b5ffad188199f75142cb21f
thecopester <thecopester@gmail.com>
2025-11-20 11:22:53 -0700

    Add files via upload

--- 3D Zipper Tube.py
+++ 3D Zipper Tube.py
@@ -4,33 +4,14 @@ from commctrl import LVM_SETITEMPOSITION32
 # INSTRUCTIONS FOR USING CODE:
 '''Put instructions here later'''
 
-# Define all parameters here:
 # Parameters pertinent to every segment in every frame:
 h = 5
 w = 10
 alpha = np.radians(60)
+# The parameters for each individual segment are defined under "if __name__ == '__main__':".
 
-# Parameters for Segment 0:
-# L_0 doesn't exist, but there will be a length for every other segment.
-theta_0 = np.radians(60)
-gamma_0 = np.radians(70)
 
-# Parameters for Segment 1:
-L_1 = 15
-theta_1 = np.radians(30)
-gamma_1 = np.radians(120)
-
-# Parameters for Segment 2:
-L_2 = 25
-theta_2 = np.radians(50)
-gamma_2 = np.radians(60)
-
-# Parameters for Segment 3:
-L_3 = 20
-theta_3 = np.radians(110)
-gamma_3 = np.radians(45)
-
-# The following functions aren't really necessary, but they make the coding faster and easier to read:
+# The following trigonometric functions make the coding faster and easier to read:
 def cos(angle):
     return np.cos(angle)
 
@@ -46,49 +27,37 @@ def csc(angle):
 def cot(angle):
     return 1/np.tan(angle)
 
-# Define the coordinates of the corners of Segment 0 (see visuals in paper to understand which corners these refer to):
-c_0_1_1 = np.array([[0],
-                    [0],
-                    [0]])
-c_0_2_1 = np.array([[w],
-                    [-w*cot(gamma_0)],
-                    [0]])
-c_0_3_1 = np.array([[w + h*cos(alpha)],
-                    [-w*cot(gamma_0) - h*cot(theta_0)],
-                    [h*sin(alpha)]])
-c_0_4_1 = np.array([[h*cos(alpha)],
-                    [-h*cot(theta_0)],
-                    [h*sin(alpha)]])
 
+# This function returns the corner coordinate for corner type n in the same frame as the segment. c_s_n_s:
 def corner_frame_s(s, n):
     L_s = parameters[0][s]
     theta_s = parameters[1][s]
     gamma_s = parameters[2][s]
 
     if n == 1:
-        c_s_1_s = np.array([[0],
+        c_s_n_s = np.array([[0],
                             [L_s],
                             [0]])
-        return c_s_1_s
+        return c_s_n_s
     elif n == 2:
-        c_s_2_s = np.array([[w],
+        c_s_n_s = np.array([[w],
                             [L_s - w * cot(gamma_s)],
                             [0]])
-        return c_s_2_s
+        return c_s_n_s
     elif n == 3:
-        c_s_3_s = np.array([[w + h * cos(alpha)],
+        c_s_n_s = np.array([[w + h * cos(alpha)],
                             [L_s - w * cot(gamma_s) - h * cot(theta_s)],
                             [h * sin(alpha)]])
-        return c_s_3_s
+        return c_s_n_s
     elif n == 4:
-        c_s_4_s = np.array([[h * cos(alpha)],
+        c_s_n_s = np.array([[h * cos(alpha)],
                             [L_s - h * cot(theta_s)],
                             [h * sin(alpha)]])
-        return c_s_4_s
+        return c_s_n_s
 
-# the individual matrix function has been verified:
+
+# This function returns the matrix needed to transform between two immediate coordinate frames. In the paper, this matrix is called T_f,f+1:
 def individual_matrix(L_f, theta_f, gamma_f):
-    # this function returns T_f,f+1 for a given frame
     u_f = cot(theta_f) - cos(alpha) * cot(gamma_f)
     a_f = csc(gamma_f)
     b_f = np.sqrt((cot(gamma_f) - cot(theta_f) * cos(alpha)) ** 2 + (sin(alpha) * csc(theta_f)) ** 2)
@@ -105,6 +74,8 @@ def individual_matrix(L_f, theta_f, gamma_f):
                              [0, 0, 0, 1]])
     return T_individual
 
+
+# This function returns the product of all individual matrices needed to transform the corner coordinates in frame s to frame f, resulting in one matrix that will be multiplied by c_s_n_s:
 def combined_matrix(parameter_list, s, f):
     # Make the parameter list such that parameters for segment 1 are in the second column
     L_list = parameter_list[0]
@@ -129,32 +100,106 @@ def combined_matrix(parameter_list, s, f):
 
     return T_combined
 
+
+# This function returns the corner coordinate of segment s, corner type n, in frame f:
 def corner_frame_f(s, n, f):
-    T = combined_matrix(parameters, s, f)
-    c_s_n_s = np.append(corner_frame_s(s,n), np.array([[1]]), axis = 0)
-    c_s_n_f = np.delete(T @ c_s_n_s, 3, axis=0)
-    return c_s_n_f
+    if s == 0 and f == 1:
+        if n == 1:
+            c_s_n_f = np.array([[0],
+                                [0],
+                                [0]])
+            return c_s_n_f
+        elif n == 2:
+            c_s_n_f = np.array([[w],
+                                [-w*cot(gamma_0)],
+                                [0]])
+            return c_s_n_f
+        elif n == 3:
+            c_s_n_f = np.array([[w + h*cos(alpha)],
+                                [-w*cot(gamma_0) - h*cot(theta_0)],
+                                [h*sin(alpha)]])
+            return c_s_n_f
+        elif n == 4:
+            c_s_n_f = np.array([[h*cos(alpha)],
+                                [-h*cot(theta_0)],
+                                [h*sin(alpha)]])
+            return c_s_n_f
+    if s == f:
+        c_s_n_f = corner_frame_s(s, n)
+        return c_s_n_f
+    if s > f:
+        T = combined_matrix(parameters, s, f)
+        c_s_n_s = np.append(corner_frame_s(s,n), np.array([[1]]), axis = 0)
+        c_s_n_f = np.delete(T @ c_s_n_s, 3, axis=0)
+        return c_s_n_f
+
 
 if __name__ == '__main__':
-    # Testing:
-    #   Verified:
-    #       c_0_n_1 coordinates
-    #       individual_matrix
-    #       corner_frame_s verified
+
+    # Parameters for Segment 0:
+    # L_0 doesn't exist, but there will be a length for every other segment.
+    theta_0 = np.radians(60)
+    gamma_0 = np.radians(70)
+
+    # Parameters for Segment 1:
+    L_1 = 15
+    theta_1 = np.radians(30)
+    gamma_1 = np.radians(120)
+
+    # Parameters for Segment 2:
+    L_2 = 25
+    theta_2 = np.radians(50)
+    gamma_2 = np.radians(60)
+
+    # Parameters for Segment 3:
+    L_3 = 20
+    theta_3 = np.radians(110)
+    gamma_3 = np.radians(45)
 
     parameters = [[0, L_1, L_2, L_3],
                   [theta_0, theta_1, theta_2, theta_3],
                   [gamma_0, gamma_1, gamma_2, gamma_3]]
 
-    print(np.append(corner_frame_s(3,4), np.array([[1]]), axis = 0))
+    c_0_1_1 = corner_frame_f(0, 1, 1)
+    c_0_2_1 = corner_frame_f(0, 2, 1)
+    c_0_3_1 = corner_frame_f(0, 3, 1)
+    c_0_4_1 = corner_frame_f(0, 4, 1)
+
+    c_1_1_1 = corner_frame_f(1, 1, 1)
+    c_1_2_1 = corner_frame_f(1, 2, 1)
+    c_1_3_1 = corner_frame_f(1, 3, 1)
+    c_1_4_1 = corner_frame_f(1, 4, 1)
+
+    c_2_1_1 = corner_frame_f(2, 1, 1)
+    c_2_2_1 = corner_frame_f(2, 2, 1)
+    c_2_3_1 = corner_frame_f(2, 3, 1)
+    c_2_4_1 = corner_frame_f(2, 4, 1)
 
     c_3_1_1 = corner_frame_f(3, 1, 1)
     c_3_2_1 = corner_frame_f(3, 2, 1)
     c_3_3_1 = corner_frame_f(3, 3, 1)
     c_3_4_1 = corner_frame_f(3, 4, 1)
 
-    print(c_3_1_1)
-    print(c_3_2_1)
-    print(c_3_3_1)
-    print(c_3_4_1)
+    print('Segment 0:')
+    print(f'c_0,1,1 = {c_0_1_1}')
+    print(f'c_0,2,1 = {c_0_2_1}')
+    print(f'c_0,3,1 = {c_0_3_1}')
+    print(f'c_0,4,1 = {c_0_4_1}\n')
 
+    print('Segment 1:')
+    print(f'c_1,1,1 = {c_1_1_1}')
+    print(f'c_1,2,1 = {c_1_2_1}')
+    print(f'c_1,3,1 = {c_1_3_1}')
+    print(f'c_1,4,1 = {c_1_4_1}\n')
+
+    print('Segment 2:')
+    print(f'c_2,1,1 = {c_2_1_1}')
+    print(f'c_2,2,1 = {c_2_2_1}')
+    print(f'c_2,3,1 = {c_2_3_1}')
+    print(f'c_2,4,1 = {c_2_4_1}\n')
+
+    print('Segment 3:')
+    print(f'c_3,1,1 = {c_3_1_1}')
+    print(f'c_3,2,1 = {c_3_2_1}')
+    print(f'c_3,3,1 = {c_3_3_1}')
+    print(f'c_3,4,1 = {c_3_4_1}\n')
