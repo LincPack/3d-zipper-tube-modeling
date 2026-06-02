0a1d09f5311c31ef038c51939853fe3a13b8e616
LincPack <lincolnkingsday@gmail.com>
2026-06-02 10:52:16 -0600

    Adding the new codebase.

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 from zipper_tube import Tube
 import numpy as np
 
-tube = Tube(0.2, 0.2, 90, 90, 90)
+tube = Tube(0.1, 0.1, 90, 90, 90)
 
 # tube.add_joint(1.2505, 85.99, 54.30)
 # tube.add_joint(1.2505, 77.99, 154.96)
@@ -10,25 +10,15 @@ tube = Tube(0.2, 0.2, 90, 90, 90)
 # tube.add_joint(1.2505, 114.63, 60.84)
 
 # This was the original tube
-# tube.get_tube_from_params(R=1.5, n=10, T=np.pi*4, alpha_deg=90, w=.2, h=.2) #n=10 for the one in the paper
+tube.get_tube_from_params(R=1.5, n=12, T=np.pi*2, offset_curve= True) #n=10 for the one in the paper
 # tube.visualize('t')
 
-tube.get_tube_from_params(R=2, n=4, T=np.pi*2, alpha_deg=90) #n=10 for the one in the paper
-# tube.visualize('t')
+# tube.get_tube_from_params(R=1, n=3, T=np.pi/2) #n=10 for the one in the paper
+tube.visualize('t')
 
 # tube.show_animation()
 # tube.create_prototypes(0.01, 1)
-# tube.print_points()
 
+tube.show_segment_midpoint_animation(3, False, True)
 
-#  54.30 85.99 1.2505
-# 2 154.96 77.99 1.2505
-# 3 55.36 101.87 1.2505
-# 4 152.20 44.56 1.2505
-# 5 60.84 114.63 1.2505
-
-a = np.array([1.41421356, -1.41421356,  4.49778714]) # Target Curve
-b = np.array([1.52341653, -1.36471617,  4.52588698]) # Actual Tube
-
-diff = a-b
-print(diff)
+# Things to try out in the code: Calculate the center of the mirror plane, then plot it as it folds up. Then I could try to make the 
